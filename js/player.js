3d09ba9398b4a5fff5eae8953e15b40b9ca15d0a
adminks666 <1635844708@qq.com>
2020-10-19 17:16:20 +0800

    Site updated: 2020-10-19 17:16:20

--- js/player.js
+++
@@ -1,7 +0,0 @@
-inject:
-  head:
-  - <link rel="stylesheet" href="/css/shadow.css">
-  - <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sviptzk/StaticFile_HEXO@latest/butterfly/css/plugins.min.css">
-  bottom:
-    - <script src="/js/shadow.js"></script>
-    - <script src="https://myhkw.cn/player/js/player.js" id="myhk" key="159611061656" m="1"></script>
