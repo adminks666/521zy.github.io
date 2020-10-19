56c66f53ff4a7b28cfc07fa46ce6c51c07f6e859
adminks666 <1635844708@qq.com>
2020-10-19 17:45:08 +0800

    Site updated: 2020-10-19 17:45:08

--- js/xw.js
+++
@@ -1,4 +0,0 @@
-// 设置页脚博主会动的心
-$(document).ready(function(e){
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> xwcker');
-})
