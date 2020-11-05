29a9e9e082a205ee28c0e9f8e28b625c5418b96d
adminks666 <1635844708@qq.com>
2020-11-05 14:49:18 +0800

    Site updated: 2020-11-05 14:49:17

---
+++ js/upjiang.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Lete on 2020/6/19.
+ */
+
+/*添加图片top*/
+var top_up = "<img style='max-width: 1000%;transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
