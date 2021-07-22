23a0d150ea6583d7f4f5f92b5928aafe34a8e94f
adminks666 <xwcker521zy@gmail.com>
2021-07-22 19:55:28 +0800

    Site updated: 2021-07-22 19:55:21

---
+++ js/f12.js
@@ -0,0 +1,16 @@
+document.onkeydown = function(){
+
+    if(window.event && window.event.keyCode == 123) {
+        alert("F12被禁用");
+        event.keyCode=0;
+        event.returnValue=false;
+    }
+    if(window.event && window.event.keyCode == 13) {
+        window.event.keyCode = 505;
+    }
+    if(window.event && window.event.keyCode == 8) {
+        alert(str+"\n请使用Del键进行字符的删除操作！");
+        window.event.returnValue=false;
+    }
+
+}
