a1cbbd4a51dbd48c7f6a86353d9bf800b43c0e96
adminks666 <xwcker521zy@gmail.com>
2021-09-09 16:47:36 +0800

    Site updated: 2021-09-09 16:47:34

--- js/huise.js
+++ js/huise.js
@@ -1,50 +1,42 @@
-	// 1.得到当前的时间（时间戳）
-	function DateFormat() {
-	  let date = new Date(),
-	    currentDate,
-	    currentTime,
-	    seperator = "-", // 如果想要其他格式 只需 修改这里
-	    month = date.getMonth() + 1,
-	    day = date.getDate(),
-	    hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours(),
-	    minute =
-	      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes(),
-	    second =
-	      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
-	  month >= 1 && month <= 9 ? (month = "0" + month) : "";
-	  day >= 0 && day <= 9 ? (day = "0" + day) : "";
-	  //当前 日期
-	  currentDate = month + seperator + day;
-	  //当前 时间
-	  currentTime = hour + ":" + minute + ":" + second;
-	  // 输出格式 为 8-27 14:45:33
-	  return currentDate + " " + currentTime;
-	}
-	var nowTime = DateFormat();
-	//1.设置九一八事变纪念日
-	var earthquake_startTime = "09-09 00:00:00";
-	var earthquake_overTime = "09-10 00:00:00";
-	//2.设置南京大屠杀纪念日
-	var earthquake_startTime = "12-13 00:00:00";
-	var earthquake_overTime = "12-14 00:00:00";	
-	// 3.将变灰封装到一个函数里，需要的时候再调用
-	function grey() {
-	  $("html").css({
-	    "-webkit-filter": "grayscale(100%)",
-	    "-moz-filter": "grayscale(100%)",
-	    "-ms-filter": "grayscale(100%)",
-	    "-o-filter": "grayscale(100%)",
-	    // ie滤镜
-	    filter: "progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)",
-	    // ie6 等低版本浏览器不需要加滤镜
-	    _filter: "none"
-	  });
-	}
-	//在这判断时间，时间字符串可以直接判断大小，不过要精确到秒
-	if (nowTime >= NJ_country_startTime && nowTime <= NJ_country_overTime) {
-	  // 4.把html 设置灰色滤镜
-	  grey();
-	} else if (nowTime >= earthquake_startTime && nowTime <= earthquake_overTime) {
-	  // 4.把html 设置灰色滤镜
-	  grey();
+if(aidaori()){
+    $("html").css({
+        "filter":"gray !important",
+        "filter":"progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)",
+        "filter":"grayscale(100%)",
+        "-webkit-filter":"grayscale(100%)",
+        "-moz-filter":"grayscale(100%)",
+        "-ms-filter":"grayscale(100%)",
+        "-o-filter":"grayscale(100%)" 
+    });
+}
+
+function aidaori(){
+    var aidaoriarr=new Array("0403","0404","0405","0406","0414","0512","0707","0807","0814","0909","0918","1213");
+    //2020年4月4日 新冠肺炎哀悼日，清明节
+    //2010年4月14日，青海玉树地震
+    //2008年5月12日，四川汶川地震
+    //2010年8月7日，甘肃舟曲特大泥石流
+    //1976年9月9日，毛泽东逝世
+	//1931年9月18日，九一八事变
+    //1937年12月13日，南京大屠杀
+	//8月14日，世界慰安妇纪念日
+	//1937年7月7日,七七事变 又称卢沟桥事变
+    var mydate = new Date();
+    var str = "";// + mydate.getFullYear();
+    var mm = mydate.getMonth()+1;
+    if(mydate.getMonth()>9){
+      str += mm;
+    }else{
+      str += "0" + mm;
+    }
+    if(mydate.getDate()>9){
+      str += mydate.getDate();
+    }else{
+      str += "0" + mydate.getDate();
+    }
+    if(aidaoriarr.indexOf(str)>-1){
+        return 1;
+    }else{
+        return 0;
+    }
 }
