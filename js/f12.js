af8b7fb2384fb32fb933b6e9598e88a10a333825
adminks666 <xwcker521zy@gmail.com>
2021-10-25 10:12:55 +0800

    Site updated: 2021-10-25 10:12:53

--- js/f12.js
+++ js/f12.js
@@ -1,16 +1,16 @@
-document.onkeydown = function(){
 
-    if(window.event && window.event.keyCode == 123) {
-        alert("F12被禁用");
-        event.keyCode=0;
-        event.returnValue=false;
-    }
-    if(window.event && window.event.keyCode == 13) {
-        window.event.keyCode = 505;
-    }
-    if(window.event && window.event.keyCode == 8) {
-        alert(str+"\n请使用Del键进行字符的删除操作！");
-        window.event.returnValue=false;
-    }
-
-}
+    //禁用右键（防止右键查看源代码） 
+    window.oncontextmenu=function(){return false;} 
+    //禁止任何键盘敲击事件（防止F12和shift+ctrl+i调起开发者工具） 
+    window.onkeydown = window.onkeyup = window.onkeypress = function () { 
+    window.event.returnValue = false; 
+        return false; 
+    } 
+    //如果用户在工具栏调起开发者工具，那么判断浏览器的可视高度和可视宽度是否有改变，如有改变则关闭本页面 
+    var h = window.innerHeight,w=window.innerWidth; 
+    window.onresize = function () { 
+        if (h!= window.innerHeight||w!=window.innerWidth){ 
+            window.close(); 
+            window.location = "about:blank"; 
+        } 
+    } 
