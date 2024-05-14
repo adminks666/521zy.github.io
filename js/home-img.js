06bf5a221aeedb498dd9ee70c9f0e03b0c8ffe9f
adminks666 <xwcker521zy@gmail.com>
2024-05-14 13:46:02 +0800

    Update home-img.js

--- js/home-img.js
+++ js/home-img.js
@@ -166,7 +166,7 @@ function createModal() {
     // 创建图片
     var modalImage = document.createElement('img');
     modalImage.id = 'modalImage';
-    modalImage.src = 'large-image.jpg'; // 默认图片路径
+    modalImage.src = 'https://pic.rmb.bdstatic.com/bjh/240510/material/ebc3df5500125e6267fe66a29fb7e916364.png'; // 默认图片路径
     modalImage.alt = '示例图片';
     modalImage.style.width = '100%';
     modalImage.style.borderRadius = '25px';
